206d8397aee5b5b1fffd2e515589e5cfbdec3a1e
Oshima Shuya <104044123+oshima27dat@users.noreply.github.com>
2024-05-24 16:07:04 +0900

    変数

--- cs0524/Form1.Designer.cs
+++ cs0524/Form1.Designer.cs
@@ -33,12 +33,15 @@
             label1 = new Label();
             textBox1 = new TextBox();
             timer1 = new System.Windows.Forms.Timer(components);
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
             button1.Font = new Font("Yu Gothic UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
-            button1.Location = new Point(317, 67);
+            button1.Location = new Point(395, 77);
             button1.Name = "button1";
             button1.Size = new Size(117, 44);
             button1.TabIndex = 0;
@@ -49,17 +52,16 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(354, 126);
+            label1.Location = new Point(535, 152);
             label1.Name = "label1";
             label1.Size = new Size(38, 15);
             label1.TabIndex = 1;
             label1.Text = "label1";
-            label1.Click += label1_Click;
             // 
             // textBox1
             // 
             textBox1.Font = new Font("Yu Gothic UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox1.Location = new Point(304, 185);
+            textBox1.Location = new Point(395, 200);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(130, 43);
             textBox1.TabIndex = 2;
@@ -71,11 +73,47 @@
             timer1.Enabled = true;
             timer1.Tick += timer1_Tick;
             // 
+            // button2
+            // 
+            button2.Font = new Font("Yu Gothic UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(598, 77);
+            button2.Name = "button2";
+            button2.Size = new Size(146, 73);
+            button2.TabIndex = 3;
+            button2.Text = "演習7-1";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Yu Gothic UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(598, 170);
+            button3.Name = "button3";
+            button3.Size = new Size(146, 73);
+            button3.TabIndex = 4;
+            button3.Text = "演習7-2";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Yu Gothic UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.Location = new Point(598, 266);
+            button4.Name = "button4";
+            button4.Size = new Size(146, 73);
+            button4.TabIndex = 5;
+            button4.Text = "演習7-4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(911, 504);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(textBox1);
             Controls.Add(label1);
             Controls.Add(button1);
@@ -91,5 +129,8 @@
         private Label label1;
         private TextBox textBox1;
         private System.Windows.Forms.Timer timer1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
--- cs0524/Form1.cs
+++ cs0524/Form1.cs
@@ -1,7 +1,11 @@
+using System.Runtime.InteropServices;
+
 namespace cs0524
 {
     public partial class Form1 : Form
     {
+        int vx = 20;
+
         public Form1()
         {
             InitializeComponent();
@@ -14,9 +18,23 @@ namespace cs0524
             MessageBox.Show("‚±‚ñ‚É‚¿‚Í" + textBox1.Text + "‚³‚ñ");
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)
         {
+            label1.Left += 1;
+            label1.Top += 1;
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int vx = 5;
+            MessageBox.Show($"{vx}");
+            vx = 10;
+            MessageBox.Show($"{vx}");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"{vx}");
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -24,10 +42,10 @@ namespace cs0524
 
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)
         {
-            label1.Left += 1;
-            label1.Top += 1;
+            vx += 10;
+            MessageBox.Show($"{vx}");
         }
     }
 }
